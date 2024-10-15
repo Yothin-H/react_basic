916611cf2a03fa5e50342981a5b165ba746f728a
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 12:38:59 +0700

    Props 2

--- react-basic/src/components/Transaction.js
+++ react-basic/src/components/Transaction.js
@@ -1,11 +1,16 @@
 import Item from "./Item";
 import './Transaction.css';
 const Transaction=()=>{
+    const data=[
+        {title:'Hospital' ,amount:'2000'},
+        {title:'Salary' ,amount:'50000'},
+        {title:'Transportation' ,amount:'30'}
+    ]
     return (
       <ul className='Item-list'>
-        <Item title='Hospital' amount='2000'/>
-        <Item title='Salary' amount='50000'/>
-        <Item title='Transportation' amount='30'/>
+        <Item title={data[0].title} amount={data[0].amount}/>
+        <Item title={data[1].title} amount={data[1].amount}/>
+        <Item title={data[2].title} amount={data[2].amount}/>
         
       </ul>
     );
