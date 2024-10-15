c9601d1674104a67e332f3899297d45a3ea1dfd2
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 11:21:50 +0700

    Components
    -Class
    -Function
    -External

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -4,12 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// test ReactDom
+// const data =(
+//   <h1>Hi eiei</h1>
+// )
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   data
+// );
+
+
+//component test
+//Function Component
+// function HelloComponent(){
+//   return <h1>Hello Component</h1>
+// }
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <HelloComponent/>
+// );
+
+//Class Component
+// class HelloComponent extends React.Component{
+//   render(){
+//     return <h1>Hello Component Class</h1>
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <HelloComponent/>
+// );
+
+
+//External Component
+import HelloComponent from './components/HelloComponent';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelloComponent/>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
