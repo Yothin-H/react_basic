72c5d309b8af2b521f6c4c15bc2d97a4fef52f71
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 14:03:00 +0700

    Basic event

--- react-basic/src/components/FormComponent.js
+++ react-basic/src/components/FormComponent.js
@@ -1,15 +1,28 @@
 import './FormComponent.css'
 const FormComponent=()=>{
+    const inputTitle=(event)=>{
+        console.log(event.target.value)
+    }
+
+    const inputAmount=(event)=>{
+        console.log(event.target.value)
+    }
+
+    const saveItem=(event)=>{
+        event.preventDefault()
+        console.log('saved')
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={saveItem}>
                 <div className="form-control">
                     <label>List</label>
-                    <input type="text" placeholder="Your list"/>
+                    <input type="text" placeholder="Your list" onChange={inputTitle}/>
                 </div>
                 <div className="form-control">
                     <label>Amount</label>
-                    <input type="text" placeholder="(+ received, - paid)"/>
+                    <input type="text" placeholder="(+ received, - paid)" onChange={inputAmount}/>
                 </div>
                 <div className="form-control">
                     <button type="submit" className='btn'>Add yours</button>
