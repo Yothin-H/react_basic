cb591174cf6ca0417a7143b646dccbd9af2c518e
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 12:55:54 +0700

    Key using

--- react-basic/src/components/Transaction.js
+++ react-basic/src/components/Transaction.js
@@ -2,15 +2,15 @@ import Item from "./Item";
 import './Transaction.css';
 const Transaction=()=>{
     const data=[
-        {title:'Hospital' ,amount:'2000'},
-        {title:'Salary' ,amount:'50000'},
-        {title:'Transportation' ,amount:'30'},
-        {title:'Assurance' ,amount:'3000'}
+        {id:1, title:'Hospital' ,amount:'2000'},
+        {id:2,title:'Salary' ,amount:'50000'},
+        {id:3,title:'Transportation' ,amount:'30'},
+        {id:4,title:'Assurance' ,amount:'3000'}
     ]
     return (
       <ul className='Item-list'>
         {data.map(element=>{
-            return <Item title={element.title} amount={element.amount} />
+            return <Item title={element.title} amount={element.amount} key={element.id} />
             // or 
             // return <Item {...element} />
         }
