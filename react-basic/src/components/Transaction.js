20a5bbd7ef5adff67465a7ac2148a82a8cede70d
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 12:48:50 +0700

    Prop and loop

--- react-basic/src/components/Transaction.js
+++ react-basic/src/components/Transaction.js
@@ -4,14 +4,15 @@ const Transaction=()=>{
     const data=[
         {title:'Hospital' ,amount:'2000'},
         {title:'Salary' ,amount:'50000'},
-        {title:'Transportation' ,amount:'30'}
+        {title:'Transportation' ,amount:'30'},
+        {title:'Assurance' ,amount:'3000'}
     ]
     return (
       <ul className='Item-list'>
-        <Item title={data[0].title} amount={data[0].amount}/>
-        <Item title={data[1].title} amount={data[1].amount}/>
-        <Item title={data[2].title} amount={data[2].amount}/>
-        
+        {data.map(element=>{
+            return <Item title={element.title} amount={element.amount} />
+        }
+        )}
       </ul>
     );
   }
