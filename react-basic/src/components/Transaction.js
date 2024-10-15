9736b70746245333dd05bd8430223f7b982fc73d
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-15 15:52:22 +0700

    Context API 2 : useContext

--- react-basic/src/components/Transaction.js
+++ react-basic/src/components/Transaction.js
@@ -1,11 +1,12 @@
 import Item from "./Item";
 import './Transaction.css';
 import DataContext from "../data/DataContext";
+import { useContext } from "react";
 
 
 const Transaction=(props)=>{
     const{items}=props
-    
+    const name =useContext(DataContext)
     return (
       <div>
         <ul className='Item-list'>
@@ -16,10 +17,7 @@ const Transaction=(props)=>{
             }
             )}
         </ul>
-        <DataContext.Consumer>
-            {value=><p>{value}</p>}
-        </DataContext.Consumer>
-        
+        {name}
       </div>
     );
   }
